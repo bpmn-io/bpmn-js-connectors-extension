da01c24c03fa09eb736800cae24e6d5e2aeea0c7
Nico Rehwaldt <git_nikku@nixis.de>
2022-03-17 20:24:39 +0100

    feat: integrate connector icon

--- connectors-extension/ConnectorsExtension.js
+++ connectors-extension/ConnectorsExtension.js
@@ -5,7 +5,9 @@ import {
 } from 'bpmn-js/lib/features/modeling/util/ModelingUtil';
 
 const appendSvg = `
-<svg width="22" height="22" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="m9.529 21 .196-8.108-7.133 4.236-1.322-2.257L8.635 11 1.27 7.128l1.322-2.256 7.133 4.236L9.53 1h2.681l-.196 8.108 7.133-4.236 1.323 2.256L13.104 11l7.366 3.871-1.323 2.257-7.133-4.236L12.21 21H9.53Z" fill="#22242A"/></svg>
+<svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M15.8125 11.0069C14.7767 11.0079 13.7689 11.343 12.9388 11.9625L10.0375 9.06127C10.6643 8.2372 11.0026 7.22976 11 6.19439C11.0056 3.96215 9.47542 2.01909 7.30408 1.50118C5.13274 0.983268 2.89029 2.02648 1.88772 4.02092C0.885144 6.01536 1.38567 8.43742 3.09667 9.87109C4.80766 11.3048 7.27994 11.3737 9.06813 10.0375L11.9625 12.9388C10.6267 14.7264 10.6952 17.1979 12.1278 18.9089C13.5605 20.6199 15.9814 21.1216 17.976 20.1207C19.9706 19.1199 21.0155 16.8791 20.5 14.7079C19.9846 12.5366 18.0441 11.0045 15.8125 11.0069ZM2.75001 6.19439C2.75001 4.29591 4.28903 2.75689 6.18751 2.75689C8.08599 2.75689 9.62501 4.29591 9.62501 6.19439C9.62501 8.09287 8.08599 9.63189 6.18751 9.63189C4.28903 9.63189 2.75001 8.09287 2.75001 6.19439Z" fill="#22242A"/>
+</svg>
 `;
 
 const appendImageUrl = 'data:image/svg+xml;utf8,' + encodeURIComponent(appendSvg);
@@ -272,7 +274,7 @@ ConnectorsExtension.prototype.getContextPadEntries = function(element) {
       entries['append-anything'] = {
         group: 'model',
         imageUrl: appendImageUrl,
-        title: translate('Append anything...'),
+        title: translate('Append connector'),
         action: {
           click: (event, element) => {
 
