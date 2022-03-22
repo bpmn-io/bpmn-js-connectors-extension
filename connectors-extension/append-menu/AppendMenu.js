5af3b5aa6c53fdce3046d4f99137e48cea29485b
Nico Rehwaldt <git_nikku@nixis.de>
2022-03-22 13:11:14 +0100

    feat: provide shared foundations for change menu
    This introduces a `ChangeMenu`, soon to replace our existing popup menu infrastructure.
    
    The component handles overall positioning and, similar to our existing popup menu, ensures there can only be one :tm:

--- connectors-extension/append-menu/AppendMenu.js
+++ connectors-extension/append-menu/AppendMenu.js
@@ -1,6 +1,5 @@
 import {
-  html,
-  render
+  html
 } from 'htm/preact';
 
 import {
@@ -10,49 +9,24 @@ import {
   useState
 } from 'preact/hooks';
 
-import {
-  domify,
-  remove as domRemove
-} from 'min-dom';
-
 import clsx from 'clsx';
 
-import '../change-menu/ChangeMenu.css';
 
-
-export default function AppendMenu(eventBus, elementTemplates, injector) {
+export default function AppendMenu(elementTemplates, injector, changeMenu) {
   this._elementTemplates = elementTemplates;
   this._injector = injector;
-
-  this._container = this._createContainer({});
-
-  eventBus.on('diagram.destroy', () => {
-    this._destroy();
-  });
+  this._changeMenu = changeMenu;
 }
 
 AppendMenu.$inject = [
-  'eventBus',
   'elementTemplates',
-  'injector'
+  'injector',
+  'changeMenu'
 ];
 
 AppendMenu.prototype._getMatchingTemplates = function(element) {
   return this._elementTemplates.getAll().filter(template => {
     return template.appliesTo.includes('bpmn:Task') || template.appliesTo.includes('bpmn:ServiceTask');
-  });
-};
-
-AppendMenu.prototype._applyTemplate = function(element, newTemplate) {
-  const commandStack = this._injector.get('commandStack'),
-        elementTemplates = this._injector.get('elementTemplates');
-
-  const oldTemplate = elementTemplates.get(element);
-
-  commandStack.execute('propertiesPanel.zeebe.changeTemplate', {
-    element: element,
-    newTemplate,
-    oldTemplate
   });
 };
 
@@ -96,35 +70,23 @@ AppendMenu.prototype._getContext = function(element) {
 AppendMenu.prototype.open = function(element, position) {
 
   const {
-    entries,
-    headerEntries
+    entries
   } = this._getContext(element);
 
-  return new Promise((resolve, reject) => {
+  const renderFn = (onClose) => html`
+    <${AppendMenuComponent}
+      entries=${ entries }
+      onClose=${ onClose }
+    />
+  `;
 
-    const onClose = () => this.reset();
-    const onSelect = (value) => {
-      onClose();
+  return this._changeMenu.open(renderFn, { position }).then((element) => {
 
-      resolve(value);
-    };
+    if (!element) {
+      return Promise.reject('user canceled');
+    }
 
-    const onCancel = () => {
-      onClose();
-
-      reject('user canceled');
-    };
-
-    render(html`
-        <${AppendMenuComponent}
-          entries=${ entries }
-          position=${ position }
-          headerEntries=${ headerEntries }
-          onSelect=${ onSelect }
-          onCancel=${ onCancel }
-        />
-      `, this._container
-    );
+    return element;
   });
 };
 
@@ -132,41 +94,20 @@ AppendMenu.prototype.isEmpty = function(element) {
   return this._getContext(element).empty;
 };
 
-AppendMenu.prototype.reset = function() {
-  render(null, this._container);
-};
-
-AppendMenu.prototype._createContainer = function(config) {
-
-  let parent = config && config.parent || 'body';
-
-  if (typeof parent === 'string') {
-    parent = document.querySelector(parent);
-  }
-
-  const container = domify('<div class="cmd-change-menu-parent"></div>');
-
-  parent.appendChild(container);
-
-  return container;
-};
-
-AppendMenu.prototype._destroy = function() {
-  domRemove(this._container);
-};
-
 
 function AppendMenuComponent(props) {
 
   const {
-    onSelect: _onSelect,
-    onCancel
+    onClose,
+    entries
   } = props;
 
-  const onSelect = (entry, shouldClose=true) => {
+  const onSelect = (entry) => {
     const selection = entry.action();
 
-    _onSelect(selection);
+    console.log(selection);
+
+    onClose(selection);
   };
 
   const inputRef = useRef();
@@ -178,8 +119,6 @@ function AppendMenuComponent(props) {
   const [ mouseSelectedTemplate, setMouseSelectedTemplate ] = useState(null);
   const [ selectedTemplate, setSelectedTemplate ] = useState(null);
 
-  console.log(props);
-
   useEffect(() => {
 
     const filter = (template) => {
@@ -187,14 +126,18 @@ function AppendMenuComponent(props) {
         return true;
       }
 
-      const search = [ template.name && 'connector' || '', template.name, template.description || '', template.label || '' ].join('---').toLowerCase();
+      const search = [
+        template.name && 'connector' || '',
+        template.name, template.description || '',
+        template.label || ''
+      ].join('---').toLowerCase();
 
       return value.toLowerCase().split(/\s/g).every(
         term => search.includes(term)
       );
     };
 
-    const templates = props.entries.filter(filter);
+    const templates = entries.filter(filter);
 
     if (!templates.includes(keyboardSelectedTemplate)) {
       setKeyboardSelectedTemplate(templates[0]);
@@ -242,7 +185,7 @@ function AppendMenuComponent(props) {
     }
 
     if (event.key === 'Escape') {
-      return onCancel();
+      return onClose();
     }
 
     // ARROW_UP or SHIFT + TAB navigation
@@ -266,68 +209,62 @@ function AppendMenuComponent(props) {
   }, []);
 
   return html`
-    <div class="cmd-change-menu">
-      <div class="cmd-change-menu__backdrop" onClick=${ onCancel }></div>
-      <div class="cmd-change-menu__overlay" style=${ `top: ${props.position.x}px; left: ${props.position.y }px` }>
+    <div class="cmd-change-menu__header">
+      <h3 class="cmd-change-menu__title">
+        Append a connector
+      </h3>
+    </div>
 
-        <div class="cmd-change-menu__header">
-          <h3 class="cmd-change-menu__title">
-            Append a connector
-          </h3>
-        </div>
+    <div class="cmd-change-menu__body">
+      <div class="cmd-change-menu__search">
+        <svg class="cmd-change-menu__search-icon" width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M9.0325 8.5H9.625L13.3675 12.25L12.25 13.3675L8.5 9.625V9.0325L8.2975 8.8225C7.4425 9.5575 6.3325 10 5.125 10C2.4325 10 0.25 7.8175 0.25 5.125C0.25 2.4325 2.4325 0.25 5.125 0.25C7.8175 0.25 10 2.4325 10 5.125C10 6.3325 9.5575 7.4425 8.8225 8.2975L9.0325 8.5ZM1.75 5.125C1.75 6.9925 3.2575 8.5 5.125 8.5C6.9925 8.5 8.5 6.9925 8.5 5.125C8.5 3.2575 6.9925 1.75 5.125 1.75C3.2575 1.75 1.75 3.2575 1.75 5.125Z" fill="#22242A"/>
+        </svg>
 
-        <div class="djs-popup-body">
-          <div class="cmd-change-menu__search">
-            <svg class="cmd-change-menu__search-icon" width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd" clip-rule="evenodd" d="M9.0325 8.5H9.625L13.3675 12.25L12.25 13.3675L8.5 9.625V9.0325L8.2975 8.8225C7.4425 9.5575 6.3325 10 5.125 10C2.4325 10 0.25 7.8175 0.25 5.125C0.25 2.4325 2.4325 0.25 5.125 0.25C7.8175 0.25 10 2.4325 10 5.125C10 6.3325 9.5575 7.4425 8.8225 8.2975L9.0325 8.5ZM1.75 5.125C1.75 6.9925 3.2575 8.5 5.125 8.5C6.9925 8.5 8.5 6.9925 8.5 5.125C8.5 3.2575 6.9925 1.75 5.125 1.75C3.2575 1.75 1.75 3.2575 1.75 5.125Z" fill="#22242A"/>
-            </svg>
-
-            <input
-              ref=${ inputRef }
-              type="text"
-              onKeyup=${ handleKey }
-              onKeydown=${ handleKeyDown }
-            />
-          </div>
-
-          <ul class="cmd-change-menu__results">
-            ${templates.map(template => html`
-              <li
-                key=${template.id}
-                class=${ clsx('cmd-change-menu__entry', { selected: template === keyboardSelectedTemplate }) }
-                onMouseEnter=${ () => setMouseSelectedTemplate(template) }
-                onMouseLeave=${ () => setMouseSelectedTemplate(null) }
-                onClick=${ (event) => { event.stopPropagation(); onSelect(template); } }
-              >
-                <div class="cmd-change-menu__entry-content">
-                  ${ template.imageUrl && html`
-                    <img src=${ template.imageUrl } />
-                  `}
-
-                  <span class=${ clsx('cmd-change-menu__name', template.className) } title="${ template.label || template.name }">
-                    ${template.label || template.name}
-                  </span>
-                  <span class="cmd-change-menu__description" title="${ template.description }">
-                    ${template.description || ''}
-                  </span>
-                </div>
-
-                <div class="cmd-change-menu__entry-help">
-                  <a href="#" title="Open element documentation" target="_blank" rel="noopener">
-                    <svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path fill-rule="evenodd" clip-rule="evenodd" d="M10.897 10.8967V6.49996H12.1663V10.8973C12.1663 11.2318 12.0385 11.5269 11.7829 11.7825C11.5273 12.0382 11.2322 12.166 10.8976 12.166H2.10234C1.74807 12.166 1.44795 12.043 1.20197 11.797C0.955995 11.5511 0.833008 11.2509 0.833008 10.8967V2.10136C0.833008 1.74709 0.955995 1.44697 1.20197 1.20099C1.44795 0.955019 1.74807 0.832031 2.10234 0.832031H6.49967V2.10136H2.10234V10.8967H10.897ZM12.1668 0.833637H7.76949L7.76886 0.833008V2.10234H10.0122L3.84375 8.27082L4.72901 9.15608L10.8975 2.9876V5.23097H12.1668V0.833637Z" fill="#818698"/>
-                    </svg>
-                  </a>
-                </div>
-              </li>
-            `)}
-
-            ${!templates.length && html`
-              <li class="cmd-change-menu__muted-entry">Nothing found</li>
-            `}
-          </ul>
-        </div>
+        <input
+          ref=${ inputRef }
+          type="text"
+          onKeyup=${ handleKey }
+          onKeydown=${ handleKeyDown }
+        />
       </div>
+
+      <ul class="cmd-change-menu__results">
+        ${templates.map(template => html`
+          <li
+            key=${template.id}
+            class=${ clsx('cmd-change-menu__entry', { selected: template === keyboardSelectedTemplate }) }
+            onMouseEnter=${ () => setMouseSelectedTemplate(template) }
+            onMouseLeave=${ () => setMouseSelectedTemplate(null) }
+            onClick=${ (event) => { event.stopPropagation(); onSelect(template); } }
+          >
+            <div class="cmd-change-menu__entry-content">
+              ${ template.imageUrl && html`
+                <img src=${ template.imageUrl } />
+              `}
+
+              <span class=${ clsx('cmd-change-menu__name', template.className) } title="${ template.label || template.name }">
+                ${template.label || template.name}
+              </span>
+              <span class="cmd-change-menu__description" title="${ template.description }">
+                ${template.description || ''}
+              </span>
+            </div>
+
+            <div class="cmd-change-menu__entry-help">
+              <a href="#" title="Open element documentation" target="_blank" rel="noopener">
+                <svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" clip-rule="evenodd" d="M10.897 10.8967V6.49996H12.1663V10.8973C12.1663 11.2318 12.0385 11.5269 11.7829 11.7825C11.5273 12.0382 11.2322 12.166 10.8976 12.166H2.10234C1.74807 12.166 1.44795 12.043 1.20197 11.797C0.955995 11.5511 0.833008 11.2509 0.833008 10.8967V2.10136C0.833008 1.74709 0.955995 1.44697 1.20197 1.20099C1.44795 0.955019 1.74807 0.832031 2.10234 0.832031H6.49967V2.10136H2.10234V10.8967H10.897ZM12.1668 0.833637H7.76949L7.76886 0.833008V2.10234H10.0122L3.84375 8.27082L4.72901 9.15608L10.8975 2.9876V5.23097H12.1668V0.833637Z" fill="#818698"/>
+                </svg>
+              </a>
+            </div>
+          </li>
+        `)}
+
+        ${!templates.length && html`
+          <li class="cmd-change-menu__muted-entry">Nothing found</li>
+        `}
+      </ul>
     </div>
   `;
 }
---
+++ connectors-extension/change-menu/ChangeMenu.js
@@ -0,0 +1,203 @@
+import './ChangeMenu.css';
+
+import {
+  html,
+  render
+} from 'htm/preact';
+
+import {
+  domify,
+  remove as domRemove
+} from 'min-dom';
+
+import {
+  useLayoutEffect,
+  useRef
+} from 'preact/hooks';
+
+import clsx from 'clsx';
+
+
+/**
+ * A modern change / append / replace menu foundation.
+ *
+ * Wraps all needed structural and UX changes (i.e. positioning logic)
+ * so components can build on top to do actual things.
+ *
+ * @param {EventBus} eventBus
+ * @param {Canvas} canvas
+ */
+export default function ChangeMenu(eventBus, canvas) {
+
+  this._canvas = canvas;
+
+  this._container = this._createContainer({});
+
+  eventBus.on('diagram.destroy', () => {
+    this._destroy();
+  });
+
+}
+
+ChangeMenu.$inject = [
+  'eventBus',
+  'canvas'
+];
+
+
+ChangeMenu.prototype.open = function(renderFn, options = {}) {
+
+  const {
+    position: _position,
+    className
+  } = options;
+
+  if (this._open) {
+    this.close();
+  }
+
+  this._open = defer();
+
+  const position = _position && (
+    (container) => this._ensureVisible(container, _position)
+  );
+
+  const onClose = result => this.close(result);
+
+  render(html`
+      <${ChangeMenuComponent}
+        onClose=${ onClose }
+        position=${ position }
+        className=${ className }
+      >
+        ${ renderFn(onClose) }
+      </${ChangeMenu}>
+    `, this._container
+  );
+
+  return this._open.promise;
+};
+
+ChangeMenu.prototype.close = function(result) {
+
+  const open = this._open;
+
+  this.reset();
+
+  this._open = null;
+
+  if (open) {
+    open.resolve(result);
+  }
+};
+
+ChangeMenu.prototype.reset = function() {
+  render(null, this._container);
+};
+
+ChangeMenu.prototype._createContainer = function(config) {
+
+  let parent = config && config.parent || 'body';
+
+  if (typeof parent === 'string') {
+    parent = document.querySelector(parent);
+  }
+
+  const container = domify('<div class="cmd-change-menu-parent"></div>');
+
+  parent.appendChild(container);
+
+  return container;
+};
+
+ChangeMenu.prototype._destroy = function() {
+  domRemove(this._container);
+};
+
+ChangeMenu.prototype._ensureVisible = function(container, position) {
+  var canvasBounds = this._canvas.getContainer().getBoundingClientRect();
+  var containerBounds = container.getBoundingClientRect();
+
+  var overAxis = {},
+      left = position.x,
+      top = position.y;
+
+  if (position.x + containerBounds.width > canvasBounds.width) {
+    overAxis.x = true;
+  }
+
+  if (position.y + containerBounds.height > canvasBounds.height) {
+    overAxis.y = true;
+  }
+
+  if (overAxis.x && overAxis.y) {
+    left = position.x - containerBounds.width;
+    top = position.y - containerBounds.height;
+  } else if (overAxis.x) {
+    left = position.x - containerBounds.width;
+    top = position.y;
+  } else if (overAxis.y && position.y < containerBounds.height) {
+    left = position.x;
+    top = 10;
+  } else if (overAxis.y) {
+    left = position.x;
+    top = position.y - containerBounds.height;
+  }
+
+  return {
+    x: left,
+    y: top
+  };
+};
+
+
+function ChangeMenuComponent(props) {
+
+  const {
+    children,
+    onClose,
+    className
+  } = props;
+
+  const overlayRef = useRef();
+
+  useLayoutEffect(() => {
+
+    if (typeof props.position !== 'function') {
+      return;
+    }
+
+    const overlayEl = overlayRef.current;
+
+    const position = props.position(overlayEl);
+
+    overlayEl.style.left = `${position.x}px`;
+    overlayEl.style.top = `${position.y}px`;
+  }, [ overlayRef.current, props.position ]);
+
+  return html`
+    <div class=${ clsx('cmd-change-menu', className) }>
+      <div class="cmd-change-menu__backdrop" onClick=${ () => onClose() }></div>
+      <div class="cmd-change-menu__overlay" ref=${ overlayRef }>
+        ${children}
+      </div>
+    </div>
+  `;
+}
+
+
+function defer() {
+
+  var resolve, reject;
+
+  var promise = new Promise((_resolve, _reject) => {
+    resolve = _resolve;
+    reject = _reject;
+  });
+
+  return {
+    promise,
+    resolve,
+    reject
+  };
+}
