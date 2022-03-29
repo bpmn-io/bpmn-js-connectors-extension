bb1713bf0964de6a794c7d1ace2d1d871cdef275
Nico Rehwaldt <git_nikku@nixis.de>
2022-03-30 01:23:33 +0200

    feat: render template task icons

--- src/ConnectorTaskRenderer.js
+++ src/ConnectorTaskRenderer.js
@@ -3,25 +3,27 @@ import inherits from 'inherits';
 import BaseRenderer from 'diagram-js/lib/draw/BaseRenderer';
 
 import {
-  getBusinessObject,
-  is
-} from 'bpmn-js/lib/util/ModelUtil';
-
-import {
   isLabel
 } from 'bpmn-js/lib/util/LabelUtil';
 
 import {
-  getStrokeColor
-} from 'bpmn-js/lib/draw/BpmnRenderUtil';
+  create as svgCreate,
+  appendTo as svgAppendTo
+} from 'tiny-svg';
+
+import {
+  sanitizeImageUrl
+} from './utils';
 
 
 export default function ConnectorTaskRenderer(
     config,
     eventBus,
-    bpmnRenderer) {
+    bpmnRenderer,
+    elementTemplates) {
 
   this._bpmnRenderer = bpmnRenderer;
+  this._elementTemplates = elementTemplates;
   this._config = config || {};
 
   BaseRenderer.call(this, eventBus, 1250);
@@ -32,26 +34,22 @@ inherits(ConnectorTaskRenderer, BaseRenderer);
 ConnectorTaskRenderer.$inject = [
   'config.bpmnRenderer',
   'eventBus',
-  'bpmnRenderer'
+  'bpmnRenderer',
+  'elementTemplates'
 ];
 
-function getElementTemplate(element) {
-  return !isLabel(element) && is(element, 'bpmn:Activity') && getBusinessObject(element).get('zeebe:modelerTemplate');
-}
+ConnectorTaskRenderer.prototype._getImageUrl = function(element) {
+  const template = this._elementTemplates.get(element);
 
-ConnectorTaskRenderer.prototype.canRender = function(element) {
-  const template = getElementTemplate(element);
-
-  return template && connectorTaskIcons[template];
+  return template && template.icon && sanitizeImageUrl(template.icon.contents);
 };
 
-const connectorTaskIcons = {
-  'io.camunda.connectors.EmailConnector-s2': [
-    'm 11.504082,24.374985 h 9.305414 V 22.249272 H 14.020232 V 18.06292 h 5.574572 V 15.937208 H 14.020232 V 12.31482 h 6.572355 v -2.12571 h -9.088505 z'
-  ],
-  'io.camunda.connectors.RestConnector-s1': [
-    'm 10.896735,24.374985 h 2.51615 v -5.552881 h 2.125711 l 3.036732,5.552881 h 2.841515 l -3.383788,-5.921627 c 1.691894,-0.585656 2.819823,-1.908804 2.819823,-4.056207 0,-3.166877 -2.27755,-4.208042 -5.227517,-4.208042 h -4.728626 z m 2.51615,-7.548447 v -4.620172 h 1.952185 c 1.952184,0 3.015041,0.563965 3.015041,2.190785 0,1.605131 -1.062857,2.429387 -3.015041,2.429387 z'
-  ]
+ConnectorTaskRenderer.prototype.canRender = function(element) {
+  if (isLabel(element)) {
+    return false;
+  }
+
+  return !isLabel(element) && this._getImageUrl(element);
 };
 
 ConnectorTaskRenderer.prototype.drawShape = function(parentGfx, element) {
@@ -59,14 +57,15 @@ ConnectorTaskRenderer.prototype.drawShape = function(parentGfx, element) {
 
   const gfx = renderer(parentGfx, element);
 
-  const paths = connectorTaskIcons[getElementTemplate(element)];
+  const img = svgCreate('image', {
+    x: 4,
+    y: 4,
+    width: 18,
+    height: 18,
+    href: this._getImageUrl(element)
+  });
 
-  for (const path of paths) {
-    this._bpmnRenderer._drawPath(parentGfx, path, {
-      fill: getStrokeColor(element, this._config.defaultStrokeColor),
-      stroke: 'none'
-    });
-  }
+  svgAppendTo(img, parentGfx);
 
   return gfx;
 };
