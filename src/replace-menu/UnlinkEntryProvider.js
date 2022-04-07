0ec6a11ca5d9d3c680fa5693d53d856d3f64a20a
Martin Stamm <martin.stamm@camunda.com>
2022-04-07 16:18:26 +0200

    feat: insert unlink entry at original position (#35)
    Ensure entry is inserted late into existing entries.

--- src/replace-menu/UnlinkEntryProvider.js
+++ src/replace-menu/UnlinkEntryProvider.js
@@ -1,40 +1,56 @@
-import * as replaceOptions from 'bpmn-js/lib/features/replace/ReplaceOptions';
 import { isDifferentType } from 'bpmn-js/lib/features/popup-menu/util/TypeUtil';
+import { getReplaceOptions } from './util/ReplaceOptionsUtil';
 
-const ALL_OPTIONS = Object.values(replaceOptions).flat();
 const REPLACE_MENU_PROVIDER = 'bpmn-replace';
-const HIGH_PRIORITY = 2000;
+const VERY_LOW_PRIORITY = 100;
 
+
+/**
+ * Injects an unlink entry into the properties panel to turn a
+ * template task into a plain BPMN task.
+ *
+ * @param {ElementTemplates} elementTemplates
+ * @param {PopupMenu} popupMenu
+ * @param {Function} translate
+ */
 export default function UnlinkEntryProvider(elementTemplates, popupMenu, translate) {
   this._elementTemplates = elementTemplates;
   this._translate = translate;
 
-  popupMenu.registerProvider(REPLACE_MENU_PROVIDER, HIGH_PRIORITY, this);
+  popupMenu.registerProvider(REPLACE_MENU_PROVIDER, VERY_LOW_PRIORITY, this);
 }
 
 /**
- * Creates a option to replace a templated task with a plain task of the same
- * type. Uses `replaceOptions` for label and icon of the entry.
+ * Returns the unlink option to insert at a specific position.
  *
  * @param {djs.model.Base} element
  *
- * @return {Array<Object>} a list of menu entry items
+ * @return { [ index, option ] } unlinkOption
  */
-UnlinkEntryProvider.prototype.getEntries = function(element) {
-  const elementTemplates = this._elementTemplates,
-        translate = this._translate;
+UnlinkEntryProvider.prototype._getUnlinkEntry = function(element, entries) {
+
+  const elementTemplates = this._elementTemplates;
+  const translate = this._translate;
 
   const currentTemplate = elementTemplates.get(element);
 
   if (!currentTemplate) {
-    return [];
+    return;
   }
+
+  const options = getReplaceOptions();
 
   const isSameType = (element, option) => !isDifferentType(element)(option);
 
-  const option = ALL_OPTIONS.find(option => isSameType(element, option));
+  const optionIndex = options.findIndex(option => isSameType(element, option));
 
-  const entry = {
+  if (optionIndex === -1) {
+    return;
+  }
+
+  const option = options[optionIndex];
+
+  const unlinkEntry = {
     id: 'replace-unlink-element-template',
     action: () => {
       elementTemplates.applyTemplate(element, null);
@@ -43,7 +59,66 @@ UnlinkEntryProvider.prototype.getEntries = function(element) {
     className: option.className
   };
 
-  return [entry];
+  // insert after previous option, if it exists
+  const previousOption = options[optionIndex - 1];
+  const previousEntry = previousOption && entries.find(([key]) => key === previousOption.actionName);
+
+  if (previousEntry) {
+    return [
+      entries.indexOf(previousEntry) + 1,
+      unlinkEntry
+    ];
+  }
+
+  // insert before next option, if it exists
+  const nextOption = options[optionIndex + 1];
+  const nextEntry = nextOption && entries.find(([key]) => key === nextOption.actionName);
+
+  if (nextEntry) {
+    return [
+      entries.indexOf(nextEntry),
+      unlinkEntry
+    ];
+  }
+
+  // fallback to insert at start
+  return [
+    0,
+    unlinkEntry
+  ];
+};
+
+
+UnlinkEntryProvider.prototype.getPopupMenuEntries = function(element) {
+
+  return (entries) => {
+
+    // convert our entries into something sortable
+    const entrySet = Object.entries(entries);
+
+    // retrieve insert entry
+    const unlinkOption = this._getUnlinkEntry(element, entrySet);
+
+    if (!unlinkOption) {
+      return entries;
+    }
+
+    const [
+      insertIndex,
+      unlinkEntry
+    ] = unlinkOption;
+
+    // insert unlink entry
+    entrySet.splice(insertIndex, 0, [ unlinkEntry.id, unlinkEntry ]);
+
+    // convert back to object
+    return entrySet.reduce((entries, [ key, value ]) => {
+      entries[key] = value;
+
+      return entries;
+    }, {});
+  };
+
 };
 
 UnlinkEntryProvider.$inject = [
