07d48a5230c6596115c27f8755bf9160c170c429
Nico Rehwaldt <git_nikku@nixis.de>
2022-06-09 09:17:09 +0200

    test: add mock specs

---
+++ test/spec/append-spec.js
@@ -0,0 +1,9 @@
+describe('append', function() {
+
+  it('should disallow for connections');
+
+  it('should disallow if ElementTemplates#createElement is missing');
+
+  it('should include <bpmn:Task> templates');
+
+});
---
+++ test/spec/keyboard-shortcuts-spec.js
@@ -0,0 +1,29 @@
+describe('keyboard shortcuts', function() {
+
+  describe('replace <R>', function() {
+
+    it('should offer replace menu, if options exist');
+
+    it('should not replace labels');
+
+  });
+
+
+  describe('append <A>', function() {
+
+    it('should offer append menu, if append target + options exist');
+
+    it('should offer create menu, if no append target exists');
+
+    it('should not append to labels');
+
+  });
+
+
+  describe('create <N>', function() {
+
+    it('should offer create menu, if no append target exists');
+
+  });
+
+});
